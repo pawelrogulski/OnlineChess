2e41cd7b3fad3cbaf9b17fa3004ad18a8336bd46
pawelrogulski <pawelrogulski@o2.pl>
2024-03-17 17:28:15 +0100

    reversing sides

--- frontend/src/app/chessboard/chessboard.component.ts
+++ frontend/src/app/chessboard/chessboard.component.ts
@@ -13,6 +13,8 @@ import { Component, OnInit } from '@angular/core';
 export class ChessboardComponent implements OnInit {
   pieces: Piece[] = [];
   chessboardButtons: { id: string; text: string; textColor: string }[][] = [];
+  whiteSide: boolean = true;//true if player is playing white pieces
+  sideColor: number[] = this.whiteSide == true ? [7,6,5,4,3,2,1,0] : [0,1,2,3,4,5,6,7];
   constructor(private displayService: DisplayService) {}
   ngOnInit(): void {
     this.loadChessPieces();
