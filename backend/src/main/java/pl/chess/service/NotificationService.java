daef956fa330fe7370086c1098ed5459961c5b8e
pawelrogulski <pawelrogulski@o2.pl>
2024-04-12 21:50:06 +0200

    disconnect handler

--- backend/src/main/java/pl/chess/service/NotificationService.java
+++ backend/src/main/java/pl/chess/service/NotificationService.java
@@ -22,4 +22,8 @@ public class NotificationService {
             emitter.completeWithError(e);
         }
     }
+
+    public void removeEmitter(UUID playerId) {
+        emitters.remove(playerId);
+    }
 }
